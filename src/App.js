1abcb4dd9f793d152c2b8fe81b36abf3dceba9f8
GODWIN-G1 <egbikwechinazom@gmail.com>
2020-08-02 13:45:32 -0700

    a full website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./Newp.css";
+import "./Main.css";
+import logo from "./assets/logo.png";
+import Header from "./Header";
+import About from "./About";
+import Features from "./Features";
+import Testimonials from "./Testimonials";
+import Client from "./Client";
+import Gallery from "./Gallery";
+import Store from "./Store";
+import Price from "./Price";
+import Frequently from "./Frequently";
+import Contactus from "./Contactus";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="logo">
+      <nav>
+        <img className="img1" src={logo} alt="logo" />
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Features</a>
+          </li>
+          <li>
+            <a href="/">Gallery</a>
+          </li>
+          <li>
+            <a href="/">Blog</a>
+          </li>
+          <li>
+            <a href="/">FAQ</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <button className="up">SIGN UP</button>
+        </ul>
+      </nav>
+      <div></div>
+      <Header />
+      <About />
+      <Features />
+      <Testimonials />
+      <Client />
+      <Gallery />
+      <Store />
+      <Price />
+      <Frequently />
+      <Contactus />
+      <Footer />
+    </section>
   );
 }
 
